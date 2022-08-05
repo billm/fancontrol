0602491cc2f2f240d2a349e4a65482419e03dd6a
Bill Marquette <bill.marquette@gmail.com>
2022-08-05 09:23:40 -0400

    Refactor some code so socket count is only pulled once. Move code that updates the fan speed to its own function. Attempt to implement a mechanism that only changes fan speed when temp moves too much. Semi-aggressively increase fan speeds every 2C increase in CPU temp, but reduce speeds only every 5C in temp. This should hopefully help stabilize CPU temps and fan speeds. This should also reduce the number of IPMI calls when the cpu temp only goes up/down by 1C or so.

--- fancontrol.go
+++ fancontrol.go
@@ -38,7 +38,11 @@ var fanLowTemp float64 = 30
 var fanHighSpeed float64 = 255
 var fanHighTemp float64 = 80
 
+// Temperature on last check
 var lastTemp float64
+
+// Temperature on last fan update
+var lastSetTemp float64
 
 // Figure out the number of CPU sockets
 func getSocketCount() int {
@@ -114,33 +118,39 @@ func runIpmiTool(args string) {
 	ipmitool.Run()
 }
 
-// Manage the fan speed
-// TODO refactor this to pull the socket code out, that logic doesn't belong in this function
-func manageFans() {
-	var fanSpeed float64 = fanLowSpeed
+func updateFanSpeed(zone string, speed float64) {
+	ipmiArgs := fmt.Sprintf("%s raw 0x30 0x91 0x5A 0x3 %s 0x%x", ipmitool, zone, int(speed))
+	runIpmiTool(ipmiArgs)
+}
 
-	numSockets := getSocketCount()
-	fmt.Printf("Found %d CPU packages\n", numSockets)
-	curTemp := getMaxPackageTemp(numSockets)
-	fmt.Printf("Current hottest CPU at %dC\n", int(curTemp))
+// Manage the fan speed
+func manageFans(curTemp float64) {
+	var fanSpeed float64 = fanLowSpeed
+	var zone = "0x10"
 
 	// Only need to change the fan speed if the temperature has changed
 	if int(curTemp) != int(lastTemp) {
-		if curTemp < fanLowTemp {
+		if curTemp <= fanLowTemp {
 			fanSpeed = fanLowSpeed
-		} else if curTemp > fanHighTemp {
+		} else if curTemp >= fanHighTemp {
 			fanSpeed = fanHighSpeed
 		} else {
 			var fanSpeedF float64 = ((curTemp - fanLowTemp) / (fanHighTemp - fanLowTemp)) * (fanHighSpeed)
 			fanSpeed = fanSpeedF
 		}
-		fmt.Printf("Setting fan speed to %d%%\n", int((fanSpeed/fanHighSpeed)*100))
 
-		// https://forums.servethehome.com/index.php?threads/supermicro-ipmi-fan-speed-control-gpu-system.23740/
-		// Zone 0 appears to be 0x10
-		var zone = "0x10"
-		ipmiArgs := fmt.Sprintf("%s raw 0x30 0x91 0x5A 0x3 %s 0x%x", ipmitool, zone, int(fanSpeed))
-		runIpmiTool(ipmiArgs)
+		// Only update fan speed if temp has gone up by 2C or more since last
+		// time it was set. Or if temp has gone down by 5C or more since last
+		// time it was set.
+		if 2 >= int(curTemp)-int(lastSetTemp) {
+			fmt.Printf("Increasing fan speed to %d%%\n", int((fanSpeed/fanHighSpeed)*100))
+			updateFanSpeed(zone, fanSpeed)
+			lastSetTemp = curTemp
+		} else if 5 >= int(lastSetTemp)-int(curTemp) {
+			fmt.Printf("Decreasing fan speed to %d%%\n", int((fanSpeed/fanHighSpeed)*100))
+			updateFanSpeed(zone, fanSpeed)
+			lastSetTemp = curTemp
+		}
 	}
 	// Cache our last temp
 	lastTemp = curTemp
@@ -175,8 +185,13 @@ func resetBmc() {
 
 func main() {
 	initializeFans()
+	numSockets := getSocketCount()
+	fmt.Printf("Found %d CPU packages\n", numSockets)
+
 	for true {
-		manageFans()
+		curTemp := getMaxPackageTemp(numSockets)
+		fmt.Printf("Current hottest CPU at %dC\n", int(curTemp))
+		manageFans(curTemp)
 		time.Sleep(time.Second * 5)
 	}
 }
